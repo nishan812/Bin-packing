349e38d349755cf75238f977e8de3da40da5af7b
Nishan Bista <nishanbista525@gmail.com>
2024-03-23 23:41:32 +0545

    sucessfully validated each group values and stored in array

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function alertMessage(msg) {
     root.style.margin = "0";
     const msgLabelBody = document.createElement("div");
     msgLabelBody.classList.add("msgLabelBody");
-    msgLabelBody.textContent =`Note: ${msg}` ;
+    msgLabelBody.textContent = `Note: ${msg}`;
     body.insertBefore(msgLabelBody, root);
 
     setTimeout(() => {
@@ -15,10 +15,6 @@ function alertMessage(msg) {
     }, 3000);
   }
 }
-
-
-
-
 
 const enterNumberBody = document.createElement("div");
 enterNumberBody.classList.add("enterNumberBody");
@@ -36,23 +32,17 @@ enterNumberBody.appendChild(numberOfGroupInput);
 
 const capacityBody = document.createElement("div");
 capacityBody.classList.add("capacityBody");
-root.appendChild(capacityBody)
+root.appendChild(capacityBody);
 
-const capacityHeading = document.createElement('p');
-capacityHeading.classList.add("capacityHeading")
-capacityHeading.textContent="Enter capacity"
-capacityBody.appendChild(capacityHeading)
+const capacityHeading = document.createElement("p");
+capacityHeading.classList.add("capacityHeading");
+capacityHeading.textContent = "Enter capacity";
+capacityBody.appendChild(capacityHeading);
 
 const capacityInput = document.createElement("input");
 capacityInput.classList.add("capacityInput");
-capacityInput.setAttribute("type","number")
-capacityBody.appendChild(capacityInput)
-
-
-
-
-
-
+capacityInput.setAttribute("type", "number");
+capacityBody.appendChild(capacityInput);
 
 const continueButton_1 = document.createElement("button");
 continueButton_1.classList.add("continueButton_1");
@@ -61,48 +51,73 @@ body.appendChild(continueButton_1);
 
 let numberOfGroups;
 let capacity;
-let groups=[];
+let groups = [];
 let lowerBound;
 
 continueButton_1.addEventListener("click", () => {
-  if (numberOfGroupInput.value.trim() == ""|| (numberOfGroupInput.value.trim()<=0) ){
+  if (
+    numberOfGroupInput.value.trim() == "" ||
+    numberOfGroupInput.value.trim() <= 0
+  ) {
     alertMessage("Number of Groups is Empty or Invalid");
-  }
-  else if(capacityInput.value.trim() == ""|| (capacityInput.value.trim()<=0)){
-    alertMessage("Capacity is Empty or Invalid")
-  }
-  else{
-    numberOfGroups=Number(numberOfGroupInput.value);
-    capacity=Number(capacityInput.value)
+  } else if (
+    capacityInput.value.trim() == "" ||
+    capacityInput.value.trim() <= 0
+  ) {
+    alertMessage("Capacity is Empty or Invalid");
+  } else {
+    numberOfGroups = Number(numberOfGroupInput.value);
+    capacity = Number(capacityInput.value);
 
-    enterNumberBody.remove()
-    capacityBody.remove()
-    continueButton_1.remove()
+    enterNumberBody.remove();
+    capacityBody.remove();
+    continueButton_1.remove();
 
     console.log(`number of groups = ${numberOfGroups}`);
     console.log(`capacity = ${capacity}`);
 
-  
     const continueButton_2 = document.createElement("button");
-    continueButton_2.textContent="Submit"
+    continueButton_2.textContent = "Submit";
     continueButton_2.classList.add("continueButton_2");
-    body.appendChild(continueButton_2)
+    body.appendChild(continueButton_2);
 
-    for(let i=1;i<=numberOfGroups;i++){
+    for (let i = 1; i <= numberOfGroups; i++) {
+      const groupBody = document.createElement("div");
+      groupBody.classList.add("groupBody");
+      root.appendChild(groupBody);
 
-        const groupBody = document.createElement("div");
-        groupBody.classList.add("groupBody");
-        root.appendChild(groupBody)
+      const groupBodyHeading = document.createElement("p");
+      groupBodyHeading.textContent = `Weight of Group ${i}`;
+      groupBody.appendChild(groupBodyHeading);
 
-        const groupBodyHeading = document.createElement("p");
-        groupBodyHeading.textContent=`Group ${i}`
-        groupBody.appendChild(groupBodyHeading)
-
-
-        console.log(i);
+      const groupInput = document.createElement("input");
+      groupInput.classList.add("groupInput");
+      groupInput.setAttribute("type", "number");
+      groupBody.appendChild(groupInput);
     }
 
+    continueButton_2.addEventListener("click", () => {
+      const groupInputs = document.querySelectorAll(".groupInput");
+      let groupInputStatus = [];
 
+      groupInputs.forEach((input) => {
+        groupInputStatus.push(input.value.trim() !== "");
+      });
+
+      if (groupInputStatus.some((data) => data === false)) {
+        alertMessage("Some Group values are empty!");
+      } else {
+        const groupValues = [];
+        groupInputs.forEach((item) => {
+          groupValues.push(Number(item.value));
+        });
+        console.log(groupValues);
+
+
+
+
+        
+      }
+    });
   }
 });
-
